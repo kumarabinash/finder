6cc71cfc00cb3bd4fcbbb47180cfcaaeb77aafb0
Kumar Abinash <abinash.b2@gmail.com>
2020-12-05 01:06:24 +0530

    Implemented search results. Implemented basic search Implemented highlights and mouse events

--- src/components/Finder/index.js
+++ src/components/Finder/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {FETCH_USERS} from "../Store/Reducers/UserReducer";
 
 import {connect} from "react-redux";
@@ -6,6 +6,9 @@ import Axios from "axios";
 
 function Finder(props){
   const [value, setValue] = useState("");
+  const [result, setResult] = useState([]);
+  const [cursor, setCursor] = useState(-1);
+  const input = useRef(null);
 
   useEffect(() => {
     Axios.get(`http://www.mocky.io/v2/5ba8efb23100007200c2750c`).then((response) => {
@@ -21,16 +24,80 @@ function Finder(props){
     search(value);
   }, [value]);
 
+  useEffect(() => {
+    setCursor(-1)
+  }, [result]);
+
   function search(query){
-    console.log("searching", query)
+    //  id: a unique id
+    //
+    // name: user’s name
+    //
+    // items: list of items ordered by user
+    //
+    // address: address of the user
+    //
+    // pincode: user address pin code
+
+    let search_result = props.data.filter(item => {
+      return item.name.toLowerCase().indexOf(query.toLowerCase()) > -1
+    });
+
+    setResult(search_result);
+
+  }
+
+  function navigate(e){
+    if([38, 40].indexOf(e.keyCode) > -1){
+      if(e.keyCode == 38){
+        console.log("handling up");
+        handleUp()
+      } else {
+        console.log("handling down");
+        handleDown()
+      }
+    }
+  }
+
+  function handleUp(){
+    if(cursor == -1){
+      return
+    }
+    setCursor(cursor => cursor - 1)
+  }
+
+  function handleDown(){
+    if(cursor == result.length -1 ){
+      return
+    }
+    setCursor(cursor => cursor + 1)
+  }
+
+  function handleMouseEnter(value) {
+    input.current.focus();
+    setCursor(value);
   }
 
   return(
     <div className="finder-container">
 
-      <input type="text" value={value} onChange={(e) => {setValue(e.target.value)}}/>
+      <input
+        type="text"
+        value={value}
+        ref={input}
+        onKeyDown={(e) => {navigate(e)}}
+        onChange={(e) => {setValue(e.target.value)}}
+      />
 
-
+      <div className="finder-results" >
+        {result.map((item, key) => {
+          return(
+            <div className={`result-item${cursor == key ? ' highlight' : ''}`} onMouseEnter={() => {handleMouseEnter(key)}}>
+              {item.name}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
